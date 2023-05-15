cdefa053c23f671dd06a16fc5f25b64e8b6d4ed1
Maxim Schardt <mxschardt@gmail.com>
2023-05-15 16:21:33 +0300

    Responsive loading added

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 import "./style.css"
 import "./workspace.css"
 
-const workspace = document.querySelector("#workspace")
-
-async function fetchTitles(limit) {
-    const uri = `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+async function fetchTitles(limit, start = 0) {
+    const uri = `https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${limit}`
     const response = await fetch(uri)
     const posts = await response.json()
     const titles = posts.map((x) => x.title)
@@ -22,5 +20,46 @@ function createCard(title) {
     return card
 }
 
-const titles = await fetchTitles(20)
+function calculateElementCount(
+    containerWidth,
+    containerHeight,
+    elementWidth,
+    elementHeight,
+    gap
+) {
+    const columns = Math.floor(containerWidth / (elementWidth + gap))
+    const rows = Math.floor(containerHeight / (elementHeight + gap))
+    return columns * rows
+}
+
+const workspace = document.querySelector("#workspace")
+const workspaceStyles = getComputedStyle(workspace)
+const cardWidth = parseFloat(workspaceStyles.getPropertyValue("--card-width"))
+const cardHeight = parseFloat(workspaceStyles.getPropertyValue("--card-height"))
+const gap = parseFloat(workspaceStyles.getPropertyValue("--gap")) / 2
+
+let cardCount = calculateElementCount(
+    workspace.offsetWidth,
+    workspace.offsetHeight,
+    cardHeight,
+    cardWidth,
+    gap
+)
+
+const titles = await fetchTitles(cardCount)
 titles.forEach((title) => workspace.appendChild(createCard(title)))
+
+addEventListener("resize", async () => {
+    const newCardCount = calculateElementCount(
+        workspace.offsetWidth,
+        workspace.offsetHeight,
+        cardHeight,
+        cardWidth,
+        gap
+    )
+    if (newCardCount > cardCount) {
+        const newTitles = await fetchTitles(newCardCount - cardCount, cardCount)
+        newTitles.forEach((title) => workspace.appendChild(createCard(title)))
+        cardCount = newCardCount
+    }
+})
